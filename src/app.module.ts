fc68237b93267a086bdeb93f08f481b4579e36c4
Matheus <matheus-rodrigues37@live.com>
2023-02-14 18:47:53 -0300

    update app.module.ts, form.controller.ts, form.module.ts and form.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FormModule } from './form/form.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [FormModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
