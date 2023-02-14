e378fb920a32862c153d6f6650ee25d99e17d886
Matheus <matheus-rodrigues37@live.com>
2023-02-14 19:15:47 -0300

    update form.controller.ts

--- src/form/form.controller.ts
+++ src/form/form.controller.ts
@@ -11,23 +11,26 @@ import { FormType } from '../@types/formType';
 @Controller()
 export class FormController {
   constructor(private readonly formService: FormService) {}
-
   @Post()
   async createForm(
     @Body() formData: FormType,
   ): Promise<{ message: string; status: number }> {
-    this.formService.createForm(formData).catch(() => {
-      throw new HttpException(
-        {
-          error: 'Email already exists',
-          status: HttpStatus.CONFLICT,
-        },
-        HttpStatus.CONFLICT,
-      );
-    });
-    return {
-      message: 'Form created successfully',
-      status: HttpStatus.CREATED,
-    };
+    return this.formService
+      .createForm(formData)
+      .then(() => {
+        return {
+          message: 'Form created successfully',
+          status: HttpStatus.OK,
+        };
+      })
+      .catch(() => {
+        throw new HttpException(
+          {
+            error: 'Email already exists',
+            status: HttpStatus.CONFLICT,
+          },
+          HttpStatus.CONFLICT,
+        );
+      });
   }
 }
